fb212167867859835cac1c7a71abaf83190f17ca
Jin <zerofield@outlook.com>
2016-04-29 07:43:53 +0800

    finish assignment

--- application2.py
+++ application2.py
@@ -5,6 +5,8 @@ import random
 import math
 import project2
 import matplotlib.pyplot as plt
+import alg_module2_graphs as graphs
+import time
 
 NODE_COUNT = 1239
 EDGE_COUNT = 3047
@@ -122,7 +124,60 @@ def random_order(graph):
 	random.shuffle(result_list)
 	return result_list
 
+def fast_targeted_order(ugraph):
 
+	ugraph = provided.copy_graph(ugraph)
+
+	n = len(ugraph)
+
+	degree_sets = dict()
+
+	for k in range(n):
+		degree_sets[k] = set([])
+
+	for node in range(n):
+		node_degree = len(ugraph[node])
+		degree_sets[node_degree].add(node)
+
+	L = list()
+
+	for k in range(n - 1, -1, -1):
+		while len(degree_sets[k]) > 0:
+			u = degree_sets[k].pop()
+
+			for neighbor in ugraph[u]:
+				degree = len(ugraph[neighbor])  
+				degree_sets[degree].remove(neighbor)
+				degree_sets[degree-1].add(neighbor)
+
+			L.append(u)
+			provided.delete_node(ugraph, u)
+
+	return L
+
+def compare_two_order_function():
+
+	m = 5
+
+	fast_order_time = list()
+	order_time = list()
+
+	for n in range(10, 1000, 10):
+		upa = UPA(n, m)
+
+		start_time = time.time()
+		fast_targeted_order(upa)
+		end_time = time.time()
+		fast_order_time.append(end_time - start_time)
+
+		start_time = time.time()
+		provided.targeted_order(upa)
+		end_time = time.time()
+		order_time.append(end_time - start_time)
+
+	plt.plot(range(10, 1000, 10), fast_order_time,'r') 
+	plt.plot(range(10, 1000, 10), order_time,'g') 
+	plt.show()
 
 def q1():
 	#network graph
@@ -147,14 +202,43 @@ def q1():
 	plt.plot(resilience2,'g') 
 	plt.plot(resilience3,'r') 
 	plt.show()
-
-def fast_targeted_order(ugraph):
-	pass
-
 	
+
+
+def q4():
+	#network graph
+	nework_graph = provided.load_graph_local('computer_network.txt')
+	order_list1 = provided.targeted_order(nework_graph)
+	resilience1 = project2.compute_resilience(nework_graph, order_list1)
+	print 'finish graph1'
+	#uer graph
+	uer_graph = UER(NODE_COUNT, 0.003999)
+	order_list2 = provided.targeted_order(uer_graph)
+	resilience2 = project2.compute_resilience(uer_graph, order_list2)
+	print 'finish graph2'
+	#upa graph
+	upa_graph = UPA(NODE_COUNT, 3)
+	order_list3 = provided.targeted_order(upa_graph)
+	resilience3 = project2.compute_resilience(upa_graph, order_list3)
+	print 'finish graph3'
+	
+	#plt.xlabel('degree')
+ 	#plt.ylabel('distribution')
+	plt.plot(resilience1,'b') 
+	plt.plot(resilience2,'g') 
+	plt.plot(resilience3,'r') 
+	plt.show()
+
+
 #test cases
 #print UER(5, 0.5)
 #print UPA(10, 5)
 #determine_p() p = 0.003999
 #determine_m() m = 3
+#print fast_targeted_order(graphs.GRAPH10)
+
+
+#questions
 #q1()
+#compare_two_order_function()
+#q4()
